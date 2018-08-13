09b560321ab989d34052db0c6177fac8604677a2
Brian Kang <brianhkang1@gmail.com>
2018-08-13 21:45:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,15 @@ katz_deli = []
 
 def line(katz_deli)
   counter = 1
+  list = []
+  katz_deli.each do |name|
+    list.push("#{counter}. #{name} ")
+  end 
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else 
-    katz_deli.each do |name|
-      puts "The line is currently: #{counter}. #{name}"
+    list.each do |name|
+      puts "The line is currently: #{name}"
 end
 
 def take_a_number(katz_deli, name)
