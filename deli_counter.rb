53b0ca43133732daf5c6cf95c3eb3e204692d5c8
Brian Kang <brianhkang1@gmail.com>
2018-08-13 21:45:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,11 @@ katz_deli = []
 def line(katz_deli)
   counter = 1
   list = []
+  
   katz_deli.each do |name|
     list.push("#{counter}. #{name} ")
   end 
+  
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else 
