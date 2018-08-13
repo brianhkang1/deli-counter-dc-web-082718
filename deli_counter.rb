3263766ef8cb9a25c4732a77f4c9b48acddefe85
Brian Kang <brianhkang1@gmail.com>
2018-08-13 21:56:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,6 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  if katz_deli.length == 0 
-    puts "The line is"
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
